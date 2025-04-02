ce2390b7e462737d5caf1a5a2d60fddd0f13a6bd
LordCode <9999269010dddd@gmail.com>
2025-04-02 17:07:41 +0300

    fix settings notifi twitch placeholder

--- bot/views/settings/notification/twitch.py
+++ bot/views/settings/notification/twitch.py
@@ -61,7 +61,7 @@ class TwitchItemModal(nextcord.ui.Modal):
         self.username = nextcord.ui.TextInput(
             label=i18n.t(locale, 'settings.notifi.twitch.modal.label'),
             placeholder=i18n.t(
-                locale, 'settings.notifi.youtube.modal.placeholder'),
+                locale, 'settings.notifi.twitch.modal.placeholder'),
         )
         self.add_item(self.username)
 
