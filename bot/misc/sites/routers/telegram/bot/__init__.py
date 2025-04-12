c34f7edccf088c4430f3e0d8cff2536f3027a3cd
LordCode <9999269010dddd@gmail.com>
2025-04-12 11:01:12 +0300

    add tg error handler

--- bot/misc/sites/routers/telegram/bot/__init__.py
+++ bot/misc/sites/routers/telegram/bot/__init__.py
@@ -1,10 +1,18 @@
-from os import getenv
+import logging
 
 from aiogram import Dispatcher
+from aiogram.types import ErrorEvent
 from . import commands, messages
 
 
 dp = Dispatcher()
+_log = logging.getLogger(__name__)
+
+
+@dp.error()
+async def error_handler(event: ErrorEvent):
+    _log.critical("Critical error caused by %s",
+                  event.exception, exc_info=True)
 
 routers = [commands.router, messages.router]
 dp.include_routers(*routers)
